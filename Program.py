31e3a993b308a90b902ae562c63c4f1557d62e2c
TheSaltyNewfie <tobydodge65@gmail.com>
2021-07-20 10:08:57 -0230

    Room one is pixel perfect

--- Program.py
+++ Program.py
@@ -5,20 +5,27 @@ import threading
 
 def markerValue(arg1):
     if arg1 == 1:
-        print("There is a person")
+        return 1
     if arg1 == 2:
-        print("There is a fire")
+        return 2
     if arg1 == 3:
-        print("There is nothing")
+        return 3
+
+
+def roomOne():
+    r.forward(366.7) #Pixel perfect for the scanner, position is also float
+    r.rotate_counterclockwise(85) # near perfect angle to read the scanner and move into the room
+    if r.read_marker() == 1:
+        r.forward(700.1) #Pixel perfect for the person in the room
+        r.rotate_clockwise(90)
+        print(r.rescue_person())
+        r.rotate_clockwise(86) # Degrees are not float so best angle here is 86
+        r.forward(724)
 
 
 r = robot.RobotController()
 r.connect()
 
-r.forward(400)
-r.rotate_counterclockwise(90)
-markerValue(r.read_marker())
-r.forward(740)
-r.rotate_clockwise(90)
-r.rescue_person()
+roomOne()
+
 
