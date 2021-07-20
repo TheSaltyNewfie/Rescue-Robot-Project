8b40effad729f3d125c47b31e283fe4eb3614340
TheSaltyNewfie <tobydodge65@gmail.com>
2021-07-20 13:25:13 -0230

    The rework file now covers room one and 2

--- Rework.py
+++ Rework.py
@@ -2,19 +2,46 @@ import robot
 r = robot.RobotController()
 r.connect()
 
-def checkIssue():
-    if r.scan_for_people() == True:
-        r.rescue_person()
-    if r.scan_for_fire() == True:
-        while r.scan_for_fire() == True:
-            r.extinguish_fire()
-
-def roomOne():
-    r.forward(366.7) 
-    r.rotate_counterclockwise(85) 
 
 def markerValue(): #If the marker reads a 1 we should do something. If it reads a 2 we skip
     if r.read_marker() == 1:
-        return
+        return True
+    if r.read_marker() == 2:
+        return False
 
+def roomOne(): #Room 1, checks for fire and people
+    if markerValue() == True:
+        r.left(650)
+        if r.scan_for_people() == True:
+            r.rescue_person()
+            r.right(670)
+            r.forward(-300)
+        if r.scan_for_fire() == True:
+            while r.scan_for_fire() == True:
+                r.extinguish_fire()
+            r.right(670)
+    if markerValue() == False:
+        pass
+
+def roomTwo(): #Room 2, checks for fire and people
+    if markerValue() == True:
+        r.left(650)
+        r.forward(85)
+        if r.scan_for_people() == True:
+            r.rescue_person()
+            r.forward(-85)
+            r.right(650)
+        if r.scan_for_fire() == True:
+            while r.scan_for_fire() == True:
+                r.extinguish_fire()
+            r.forward(-85)
+            r.right(650)
+    if markerValue() == False:
+        pass
+
+#These are the movement commands
+r.forward(370)
+r.left(100)
 roomOne()
+r.forward(850)
+roomTwo()
