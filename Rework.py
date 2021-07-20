19deedfc2fbf425baa988990f9c0e54124ad0432
TheSaltyNewfie <tobydodge65@gmail.com>
2021-07-20 14:40:53 -0230

    Room 3-4 are now implemented, 4 is WIP

--- Rework.py
+++ Rework.py
@@ -16,10 +16,16 @@ def roomOne(): #Room 1, checks for fire and people
             r.rescue_person()
             r.right(670)
             r.forward(-300)
-        if r.scan_for_fire() == True:
+            r.forward(300)
+            print("Finished RoomOne")
+        elif r.scan_for_fire() == True:
             while r.scan_for_fire() == True:
                 r.extinguish_fire()
             r.right(670)
+        else:
+            pass #Remove when you test temp code this is here to not throw errors right now
+            #! Please put checking temp here
+            r.right(670)#! Leave this here
     if markerValue() == False:
         pass
 
@@ -31,17 +37,60 @@ def roomTwo(): #Room 2, checks for fire and people
             r.rescue_person()
             r.forward(-85)
             r.right(650)
-        if r.scan_for_fire() == True:
+            r.forward(-850)
+        elif r.scan_for_fire() == True:
             while r.scan_for_fire() == True:
                 r.extinguish_fire()
             r.forward(-85)
             r.right(650)
+        else:
+            pass #Remove when you test temp code this is here to not throw errors right now
+            #! Copy from room one
+            r.forward(-85)#! Leave this here
+            r.right(650)#! Leave this here
+    if markerValue() == False:
+        pass
+
+def roomThree(): #Room 2, checks for fire and people
+    if markerValue() == True:
+        r.rotate_counterclockwise(90)
+        r.forward(100)
+        #Put temperature code here
+        r.forward(-100)
+        r.rotate_clockwise(90)
+    if markerValue() == False:
+        pass
+
+def roomFour(): #Room 2, checks for fire and people
+    if markerValue() == True:
+        pass
+        #r.rotate_counterclockwise(90)
+        #r.forward(100)
+        #Put temperature code here
+        #r.forward(-100)
+        #r.rotate_clockwise(90)
     if markerValue() == False:
         pass
 
 #These are the movement commands
 r.forward(370)
 r.left(100)
+print(f'This room ID is: {r.read_marker()}')
 roomOne()
-r.forward(850)
+r.forward(550)
+print(f'This room ID is: {r.read_marker()}')
 roomTwo()
+r.forward(430)
+r.left(1580)
+r.rotate_counterclockwise(90)
+print(f'This room ID is: {r.read_marker()}')
+roomThree()
+r.forward(600)
+r.rotate_counterclockwise(90)
+r.forward(800)
+r.rotate_counterclockwise(90)
+r.forward(690)
+r.rotate_clockwise(90)
+r.forward(850)
+print(f'This room ID is: {r.read_marker()}')
+roomFour()
