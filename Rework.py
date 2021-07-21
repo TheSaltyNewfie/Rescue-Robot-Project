f87133986c9b25dfc0dcde6c54b899904a496667
TheSaltyNewfie <tobydodge65@gmail.com>
2021-07-21 10:02:09 -0230

    Comments are changed

--- Rework.py
+++ Rework.py
@@ -26,7 +26,11 @@ def roomOne(): #Room 1, checks for fire and people
             r.right(670)
         else: # Use the else statement for temperature code I think we may change it from else to do it anyway
             pass 
+
+
             #TODO: Put temperature code here
+
+            
             r.right(670)#! Leave this here, it tells the bot how to leave
     if markerValue() == False:
         pass
@@ -47,7 +51,11 @@ def roomTwo(): #Room 2, checks for fire and people
             r.right(650)
         else:
             pass
+
+
             #TODO: Put temperature code here
+
+
             r.forward(-85)#! Leave this here, it tells the bot how to leave
             r.right(650)#! Leave this here, it tells the bot how to leave
     if markerValue() == False:
@@ -57,7 +65,11 @@ def roomThree(): #Room 3, checks for fire and people
     if markerValue() == True:
         r.rotate_counterclockwise(90)
         r.forward(100)
+
+
         #TODO: Put temperature code here
+
+
         r.forward(-100)#! Leave this here, it tells the bot how to leave
         r.rotate_clockwise(90)#! Leave this here, it tells the bot how to leave
     if markerValue() == False:
@@ -65,9 +77,28 @@ def roomThree(): #Room 3, checks for fire and people
 
 def roomFour(): #Room 4, checks for fire and people
     if markerValue() == True:
-        pass
-        r.forward(100)
-        #temperature code here
+        r.forward(300)
+        r.left(200)
+        r.rotate_counterclockwise(90)
+        if r.scan_for_people() == True:
+            r.rescue_person()
+            r.forward(-200)
+            r.left(-300)
+            print("Finished RoomOne")
+        elif r.scan_for_fire() == True:
+            while r.scan_for_fire() == True:
+                r.extinguish_fire()
+            r.forward(-200)
+            r.left(-300)
+        else: # Use the else statement for temperature code I think we may change it from else to do it anyway
+            pass 
+
+
+            #TODO: Put temperature code here
+
+
+            r.forward(-200)#! Leave this here, it tells the bot how to leave
+            r.left(-300)#! Leave this here, it tells the bot how to leave
     if markerValue() == False:
         pass
 
@@ -87,9 +118,7 @@ roomThree()
 r.forward(600)
 r.rotate_counterclockwise(90)
 r.forward(800)
-r.rotate_counterclockwise(90)
-r.forward(690)
-r.rotate_clockwise(90)
+r.left(690)
 r.forward(850)
 print(f'This room ID is: {r.read_marker()}')
 roomFour()
